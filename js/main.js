e694dea07589375ebeb6e50382ac121198af2c56
Natalia <y_natalya_v@mail.ru>
2021-06-02 13:13:24 +0300

    module2-task1 - fix-настройка WB

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
 const getRandomInteger = function (min, max) {
   if (min >= 0 && max >= 0) {
     return Math.floor(Math.random() * (max - min + 1) + min);
-  } return null;
+  }
+  return null;
 };
 getRandomInteger(-4, 56);
 
