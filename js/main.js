8dc77275c881909dcd72240c329715d7dcb341ac
Natalia <y_natalya_v@mail.ru>
2021-06-08 13:25:18 +0300

    module4-task1 - more details

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
+const AUTHORS = ['img/avatars/user{01}.png', 'img/avatars/user{02}.png', 'img/avatars/user{03}.png', 'img/avatars/user{04}.png',
+  'img/avatars/user{05}.png', 'img/avatars/user{06}.png', 'img/avatars/user{07}.png', 'img/avatars/user{08}.png', 'img/avatars/user{09}.png',
+  'img/avatars/user{10}.png'];
+const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const CHECKIN = ['12:00', '13:00', '14:00'];
+const CHECKOUT = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const SIMILAR_AD_COUNT = 10;
+const ADDRESS_LOCATION_X = 60;
+const ADDRESS_LOCATION_Y = 590;
+const PRICES = {
+  PRICE_MIN: 500,
+  PRICE_MAX: 5000,
+};
+const ROOMS = {
+  ROOM_MIN: 1,
+  ROOM_MAX: 10,
+};
+const GUESTS = {
+  GUESTS_MIN: 1,
+  GUESTS_MAX: 10,
+};
+const LOCATION_X = {
+  LAT_X_MIN: 35.65000,
+  LAT_X_MAX: 35.70000,
+};
+const LOCATION_Y = {
+  LNG_Y_MIN: 35.65000,
+  LNG_Y_MAX: 35.70000,
+};
+const DIGIT = 5;
+
 // Функция, возвращающая случайное число
 const getRandomInteger = function (min, max) {
   if (min >= 0 && max >= 0) {
@@ -5,7 +40,6 @@ const getRandomInteger = function (min, max) {
   }
   return null;
 };
-getRandomInteger(-4, 56);
 
 // Функция, возвращающая случайное число с плавающей точкой
 const getRandomFloat = function (min, max, digit) {
@@ -16,4 +50,46 @@ const getRandomFloat = function (min, max, digit) {
     return randomNum.toFixed(digit);
   }
 };
-getRandomFloat(1.2, 10.3, 2);
+
+// Функция, возвращаяющая рандомный элемент
+const getRandomArrayElement = function (elements) {
+  return elements[getRandomInteger(0, elements.length - 1)];
+};
+
+// Функция, возвращает строку из массива рандомной длины
+const createRandomString = function (arr, length) {
+  const newArr = [];
+  for (let i = 0; i < length; i += 1) {
+    newArr.push(FEATURES[i]);
+  }
+  return newArr.join(', ');
+};
+
+// Функция, генерирующая массив из объектов
+const createAd = function () {
+  return {
+    author: {
+      avatar: getRandomArrayElement(AUTHORS),
+    },
+    offer: {
+      title: 'Домик у моря',
+      address: `${getRandomInteger(ADDRESS_LOCATION_X, ADDRESS_LOCATION_Y)} ${getRandomInteger(ADDRESS_LOCATION_X, ADDRESS_LOCATION_Y)}`,
+      price: getRandomInteger(PRICES.PRICE_MIN, PRICES.PRICE_MAX),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomInteger(ROOMS.ROOM_MIN, ROOMS.ROOM_MAX),
+      guests: getRandomInteger(GUESTS.GUESTS_MIN, GUESTS.GUESTS_MAX),
+      checkin: getRandomArrayElement(CHECKIN),
+      checkout: getRandomArrayElement(CHECKOUT),
+      features: createRandomString(FEATURES, (getRandomInteger(0, FEATURES.length - 1))),
+      description: 'Самое романтичное место на Земле',
+      photos: getRandomArrayElement(PHOTOS),
+    },
+    location: {
+      lat: getRandomFloat(LOCATION_X.LAT_X_MIN, LOCATION_X.LAT_X_MAX, DIGIT),
+      lng: getRandomFloat(LOCATION_Y.LNG_Y_MIN, LOCATION_Y.LNG_Y_MAX, DIGIT),
+    },
+  };
+};
+
+// КОММЕНТ ДЛЯ НАСТАВНИКА ПОСЛЕ КОНСУЛЬТАЦИИ - ОШИБКА В НЕИСПОЛЬЗОВАНИИ ПЕРЕМЕННОЙ
+const similarAds = new Array(SIMILAR_AD_COUNT).fill(null).map(() => createAd());
