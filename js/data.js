a96aa9ce44a1c9ea76aedc5d3c42b1679a19d28a
Natalia <y_natalya_v@mail.ru>
2021-06-23 21:07:06 +0300

    module5-task1 - Разбивка на модули

--- js/data.js
+++ js/data.js
@@ -1,7 +1,5 @@
 import { getRandomPositiveInteger, getRandomPositiveFloat, getRandomArrayElement } from './util.js';
 
-const AUTHORS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
 const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 
 const CHECKIN = ['12:00', '13:00', '14:00'];
@@ -20,19 +18,24 @@ const ADDRESS_LOCATION_X = 60;
 
 const ADDRESS_LOCATION_Y = 590;
 
+const Authors = {
+  MIN: 1,
+  MAX: 10,
+};
+
 const Prices = {
-  PRICE_MIN: 500,
-  PRICE_MAX: 5000,
+  MIN: 500,
+  MAX: 5000,
 };
 
 const Rooms = {
-  ROOM_MIN: 1,
-  ROOM_MAX: 10,
+  MIN: 1,
+  MAX: 10,
 };
 
 const Guests = {
-  GUESTS_MIN: 1,
-  GUESTS_MAX: 10,
+  MIN: 1,
+  MAX: 10,
 };
 
 const LocationX = {
@@ -50,35 +53,40 @@ const DIGIT = 5;
 // Функция, возвращает массив рандомной длины
 const getRandomArrayLength = function (arr, length) {
   const newArr = [];
+
   for (let i = 0; i < length; i += 1) {
     newArr.push(FEATURES[i]);
   }
+
   return newArr;
+
 };
 
-// Функция, создающая строку с рандомным значением
-const getRandomConcatString = function () {
-  const newString = getRandomArrayElement(AUTHORS);
-  if (newString < AUTHORS.length) {
-    return `img/avatars/user0${newString}.png`;
-  } else if (newString >= AUTHORS.length) {
-    return `img/avatars/user${newString}.png`;
+// Функция, создающая аватар пользователя
+const getPathOfAuthorsAvatar = function (elem) {
+
+  if (elem === Authors.MAX - 1) {
+    return elem + 1;
   }
+
+  return `0${elem + 1}`;
+
 };
 
 // Функция, генерирующая массив из объектов
-const createAd = function () {
+const createAd = function (result) {
+
   return {
     author: {
-      avatar: getRandomConcatString(AUTHORS),
+      avatar: `img/avatar${getPathOfAuthorsAvatar(result)}.png`,
     },
     offer: {
       title: 'Домик у моря',
       address: `${getRandomPositiveInteger(ADDRESS_LOCATION_X, ADDRESS_LOCATION_Y)} ${getRandomPositiveInteger(ADDRESS_LOCATION_X, ADDRESS_LOCATION_Y)}`,
-      price: getRandomPositiveInteger(Prices.PRICE_MIN, Prices.PRICE_MAX),
+      price: getRandomPositiveInteger(Prices.MIN, Prices.MAX),
       type: getRandomArrayElement(TYPES),
-      rooms: getRandomPositiveInteger(Rooms.ROOM_MIN, Rooms.ROOM_MAX),
-      guests: getRandomPositiveInteger(Guests.GUESTS_MIN, Guests.GUESTS_MAX),
+      rooms: getRandomPositiveInteger(Rooms.MIN, Rooms.MAX),
+      guests: getRandomPositiveInteger(Guests.MIN, Guests.MAX),
       checkin: getRandomArrayElement(CHECKIN),
       checkout: getRandomArrayElement(CHECKOUT),
       features: getRandomArrayLength(FEATURES, (getRandomPositiveInteger(0, FEATURES.length - 1))),
@@ -93,5 +101,5 @@ const createAd = function () {
 };
 
 // КОММЕНТ ДЛЯ НАСТАВНИКА ПОСЛЕ КОНСУЛЬТАЦИИ - ОШИБКА ПЕРЕМЕННОЙ similarAds, которая представляет из себя массив с данными, но не используется пока
-const similarAds = new Array(SIMILAR_AD_COUNT).fill(null).map(() => createAd());
-export { similarAds };
+const similarAds = new Array(SIMILAR_AD_COUNT).fill(null).map((element, index) => createAd(index));
+console.log(similarAds);
