af64bb2a6afb519bb58afa188698e73ec3a789df
Natalia <y_natalya_v@mail.ru>
2021-06-25 09:56:36 +0300

    Модуляция (#5)
    * Update Readme.md
    
    * module5-task1 - module1
    
    * module5-task1 - разбивка на модули
    
    * module5-task1 - разбивка на модули
    
    * module5-task1 - Разбивка на модули
    
    * module5-task1 - Разбивка на модули
    
    * module5-task1 - Разбивка на модули - правки

---
+++ js/util.js
@@ -0,0 +1,26 @@
+/*УТИЛИТАРНЫЙ МОДУЛЬ*/
+
+// Функция взята из интернета и доработана
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+const getRandomPositiveInteger = function (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+// Функция взята из интернета и доработана
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+const getRandomPositiveFloat = function (a, b, digits = 1) {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (upper - lower) + lower;
+  return result.toFixed(digits);
+};
+
+// Функция, возвращаяющая рандомный элемент
+const getRandomArrayElement = function (elements) {
+  return elements[getRandomPositiveInteger(0, elements.length - 1)];
+};
+
+export {getRandomPositiveInteger, getRandomPositiveFloat, getRandomArrayElement};
